c0f1c5036d1c1a7e79762be7719dc9df6660ff26
Syed Roshan <toroshaninbox@gmail.com>
2025-04-29 17:17:46 +0530

    Fixed home page to use BannerSlider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
 import BannerSlider from "@/components/BannerSlider";
+import CallToAction from "@/components/CallToAction";
+import AboutProgram from "@/components/AboutProgram";
+import WhySupport from "@/components/WhySupport";
+import HowItWorks from "@/components/HowItWorks";
+
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
+      <Banner />
       <BannerSlider />
+      <CallToAction />
+      <AboutProgram />
+      <WhySupport />
+      <HowItWorks />
+      
     </main>
   );
 }
